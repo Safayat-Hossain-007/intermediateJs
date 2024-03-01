7c8ec85fda1c05b4ca17ac8e53936fad041b0bdf
Safayat <safayathossain40@gmail.com>
2024-03-01 23:09:03 +0600

    Commit

--- factorialWhile.js
+++ factorialWhile.js
@@ -1,5 +1,11 @@
-var i =0;
-while(i<10){
-    console.log(i);
-    i++;
+function factorial(number){
+    var fact=1;
+    var i = 1;
+    while(i<=number){
+        fact =fact*i;
+        // console.log(i,fact);
+        i++;
+    }
+    return fact;
 }
+console.log(factorial(5));
