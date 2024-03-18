2aab50ed857ebe80360b823359ad22915d95d067
Safayat <safayathossain40@gmail.com>
2024-03-18 22:11:15 +0600

    IIFE

---
+++ functionGeneral.js
@@ -0,0 +1,11 @@
+function addToDo(task){
+    const newTaskElement = document.createElement('li');//local variable
+}
+
+let name1 = 'Sadat';//Global variable
+
+(function(){
+    var  RomanticName = "Faltu";
+    console.log(name1,RomanticName);
+
+ })();//Immediately Invoking Function Expression IIFE
